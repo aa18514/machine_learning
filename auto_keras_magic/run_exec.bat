cf12427488218218bbb92fa35227623288552daf
aa18514 <aa18514@github.com>
2018-08-16 23:26:51 +0100

    update bat

--- auto_keras_magic/run_exec.bat
+++ auto_keras_magic/run_exec.bat
@@ -1,6 +1,9 @@
 @echo off 
+::set up virtual environment folder
 virtualenv venv
 call venv\Scripts\activate.bat
 pip install -r requirements.txt
 python auto_keras_magic.py
 call venv\Scripts\deactivate.bat
+::remove virtual environment folder
+rm -rf venv
