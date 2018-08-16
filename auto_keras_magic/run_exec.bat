9e09b70ec665ec147dfadf3e05ca979c350153f3
aa18514 <aa18514@github.com>
2018-08-16 19:40:52 +0100

    nice bat file

--- auto_keras_magic/run_exec.bat
+++ auto_keras_magic/run_exec.bat
@@ -1,2 +1,5 @@
 @echo off 
-cmd /k "cd /d C:\Users\user\Desktop\machine_learning\machine_learning\auto_keras_magic\venv\Scripts & activate & cd /d C:\Users\user\Desktop\machine_learning\machine_learning\auto_keras_magic & pip install -r requirements.txt & cd /d    C:\Users\user\Desktop\machine_learning\machine_learning\auto_keras_magic & python auto_keras_magic.py"
+virtualenv venv
+call venv\Scripts\activate.bat
+pip install -r requirements.txt
+python auto_keras_magic.py
