8194471f512464483a2801d3aa599b6a7a33ab99
aa18514 <aa18514@github.com>
2018-10-09 10:46:46 +0100

    typo

--- stock_price_prediction/train_model.py
+++ stock_price_prediction/train_model.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 def create_keras_regressor(neurons=[20, 20, 20, 20, 1],
                  dropout=[0.01, 0.01, 0.01, 0.01, 0.00],
-                 activations='relu', 'relu', 'relu', 'relu', 'linear',
+                 activations=['relu', 'relu', 'relu', 'relu', 'linear'],
                  loss='mse'):
     model = Sequential()
     for i in range(len(dropout)):
@@ -45,7 +45,7 @@ def train_mlp_classifier(X_train, Y_train, X_test):
     model = create_keras_classifier()
     history = model.fit(X_train, Y_train, validation_split=0.33, epochs=150)
     plot_accuracy(history)
-    #X_te = model.predict_proba(X_test)
+    X_te = model.predict_proba(X_test)
     X_te[X_te >= 0.50] = 1
     X_te[X_te < 0.50] = 0
     return X_te, model.predict_classes(X_train)
