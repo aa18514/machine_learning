51a879b4e8eb36476a248ebf136e0d31fbaf0e49
aa18514 <aa18514@github.com>
2018-11-12 23:00:59 +0500

    change to class

--- stock_price_prediction/train_model.py
+++ stock_price_prediction/train_model.py
@@ -7,89 +7,88 @@ import sklearn.svm as svm
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import GridSearchCV
 import matplotlib.pyplot as plt
-import yaml
 import numpy as np
-
-def load_model(model_file='model\\regressor.yml'):
-    cfg = None
-    with open(model_file, 'r') as ymlfile:
-        cfg = yaml.load(ymlfile)
-    return cfg
-
-
-def create_keras_model(nn):
-    model = Sequential()
-    for layer in nn['layers']:
-        if layer == 'BatchNormalization':
-            model.add(BatchNormalization())
-        else:
-            for key, value in layer.items():
-                if value['type'] == 'feedforward':
-                    model.add(Dense(units=int(value['nuerons']), activation=value['activation']))
-                    model.add(Dropout(float(value['dropout'])))
-    if nn['optimizer'] == 'Adam':
-        adam_optimizer = Adam(lr=nn['learning_rate'])
-        model.compile(loss=nn['loss'], optimizer=nn['optimizer'], metrics=nn['metric'])
-        return model
-    return None
-
-
-def plot_accuracy(history, attributes=['acc', 'loss']):
-    for attr in attributes:
-        plt.xlabel('epochs')
-        plt.ylabel(attr)
-        plt.plot(history.history[attr], label='train')
-        plt.plot(history.history['val_' + attr], label='test')
-        plt.legend()
-        plt.show()
-
-
-def train_mlp_classifier(X_train, Y_train, X_test):
-    cfg = load_model('model\\classifier.yml')['nn']
-    model = create_keras_model(cfg)
-    history = model.fit(X_train, Y_train, validation_split=float(cfg['validation_split']), epochs=int(cfg['epochs']))
-    plot_accuracy(history)
-    return model.predict_classes(X_test), model.predict_classes(X_train)
-
-
-def train_mlp_regressor(X_train, Y_train, X_test):
-    cfg = load_model()['nn']
-    model = create_keras_model(cfg)
-    history = model.fit(X_train, Y_train, validation_split=float(cfg['validation_split']), epochs=int(cfg['epochs']))
-    predictions = model.predict(X_test)
-    return predictions, model.predict(X_train)
-
+from keras.layers import GRU
+from keras import regularizers
 import time
-def rolling_mlp_regressor(X_train, Y_train, X_test, Y_test, ori):
-    cfg = load_model()['nn']
-    model = create_keras_model(cfg)
-    predictions = []
-    errors = []
-    for i in range(len(X_test)):
-        history = model.fit(X_train, Y_train, validation_split=float(cfg['validation_split']), epochs=int(cfg['epochs']))
-        print(X_test[i])
-        prediction = model.predict(X_test)[i]
-        predictions.append(prediction)
-        print(X_train.shape)
-        X_train = list(X_train)
-        Y_train = list(Y_train)
-        X_train.append(X_test[i])
-        Y_train.append(Y_test[i])
-        X_train = np.array(X_train)
-        Y_train = np.array(Y_train)
-        errors.append(np.abs(ori[i] * Y_test[i] - ori[i] * prediction))
-        print("mae {}".format(np.abs(ori[i] * Y_test[i] -  ori[i] * prediction)))
-        print("length of ori {}".format(len(ori)))
-        print("length of Y_test {}".format(len(Y_test)))
-        #time.sleep(1)
-    plt.ylabel('error in $')
-    plt.xlabel('days')
-    plt.plot(errors)
-    plt.show()
-    Y_pred = np.array(predictions)
-    Y_pred[Y_pred < 1] = 0
-    Y_pred[Y_pred > 1] = 1
-    return Y_pred, model.predict(X_train)
+
+
+class nueral_network:
+    def __init__(x_train, y_train, x_test, y_test, nn):
+        self._x_train = x_train
+        self._y_train = y_train
+        self._x_test = x_test
+        self._y_test = y_test
+        self._nn = nn
+
+
+    def create_keras_model():
+        model = Sequential()
+        for layer in self._nn['layers']:
+            if layer == 'BatchNormalization':
+                model.add(BatchNormalization())
+            else:
+                for key, value in layer.items():
+                    model.add(Dense(units=int(value['nuerons']), activation=value['activation'],\
+                        kernel_regularizer=regularizers.l2(float(value['kernel_regularizer']))))
+                    model.add(Dropout(float(value['dropout'])))
+                    if value['type'] == 'gru':
+                        model.add(GRU(10, return_sequences=True))
+        if nn['optimizer'] == 'Adam':
+            adam_optimizer = Adam(lr=self._nn['learning_rate'])
+            model.compile(loss=self._nn['loss'], optimizer=self._nn['optimizer'], metrics=self._nn['metric'])
+            return model
+        return None
+
+
+    def plot_accuracy(history, attributes=['acc', 'loss']):
+        for attr in attributes:
+            plt.xlabel('epochs')
+            plt.ylabel(attr)
+            plt.plot(history.history[attr], label='train')
+            plt.plot(history.history['val_' + attr], label='test')
+            plt.legend()
+            plt.show()
+
+
+    def train_mlp_classifier():
+        model = self.create_keras_model()
+        history = model.fit(self._x_train, self._y_train, validation_split=float(self._nn['validation_split']),\
+                epochs=int(self._nn['epochs']))
+        self.plot_accuracy(history)
+        return model.predict_classes(self._x_test), model.predict_classes(self._x_train)
+
+
+    def train_mlp_regressor():
+        model = self.create_keras_model()
+        history = model.fit(self._x_train, self._y_train, validation_split=float(self._nn['validation_split']),\
+                epochs=int(self._nn['epochs']))
+        return model.predict(self._x_test), model.predict(self._x_train)
+
+
+    def rolling_mlp_regressor(ori):
+        model = self.create_keras_model()
+        predictions = []
+        errors = []
+        for i in range(len(self._x_test)):
+            history = model.fit(self._x_train, self._y_train, validation_split=float(self._nn['validation_split']), epochs=int(self._nn['epochs']))
+            prediction = model.predict(self._x_test)[i]
+            predictions.append(prediction)
+            x_train = list(self._x_train)
+            y_train = list(self._y_train)
+            x_train.append(self._x_test[i])
+            y_train.append(self._y_test[i])
+            self._x_train = np.array(x_train)
+            self._y_train = np.array(y_train)
+            errors.append(np.abs(ori[i] * self._y_test[i] - ori[i] * prediction))
+            print("mae {}".format(np.abs(ori[i] * self._y_test[i] -  ori[i] * prediction)))
+            print("length of ori {}".format(len(ori)))
+            print("length of Y_test {}".format(len(self._y_test)))
+        plt.ylabel('error in $')
+        plt.xlabel('days')
+        plt.plot(errors)
+        plt.show()
+        return np.array(predictions), model.predict(self._x_train)
 
 
 def svm_regressor(X_train, Y_train, X_test):
